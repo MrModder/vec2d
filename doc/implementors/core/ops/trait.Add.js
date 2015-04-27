2f2ffe28db32d02f84b7cf4d70cf622ead6d1f23
Nicholas Bishop <nicholasbishop@gmail.com>
2015-04-27 03:04:05 -0400

    Automatic-ish rustdoc update

---
+++ doc/implementors/core/ops/trait.Add.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['vec2d'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Add.html' title='core::ops::Add'>Add</a> for <a class='struct' href='vec2d/struct.Coord.html' title='vec2d::Coord'>Coord</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
