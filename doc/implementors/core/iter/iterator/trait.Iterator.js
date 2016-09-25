76ccd16ef91841be06acb0d77823764a8d68a401
Nicholas Bishop <nicholasbishop@gmail.com>
2016-09-25 18:25:17 -0400

    Automatic-ish rustdoc update

---
+++ doc/implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vec2d"] = ["impl&lt;'a,&nbsp;Elem&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec2d/struct.RectIter.html' title='vec2d::RectIter'>RectIter</a>&lt;'a,&nbsp;Elem&gt;","impl&lt;'a,&nbsp;Elem&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='vec2d/struct.RectIterMut.html' title='vec2d::RectIterMut'>RectIterMut</a>&lt;'a,&nbsp;Elem&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
